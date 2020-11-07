8919f6147481569fcc30c596b3bc3062696d908c
dpm24 <dpm2448dpm@gmail.com>
2020-11-07 19:17:28 +0100

    first

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Seleccione el archivo json a subir' });
+});
+
+module.exports = router;
