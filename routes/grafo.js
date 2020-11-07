00295bc8a094b74d0ac9c74996a1b61e40064cd2
dpm24 <dpm2448dpm@gmail.com>
2020-11-07 19:38:53 +0100

    view

--- routes/grafo.js
+++ routes/grafo.js
@@ -1,2 +1,9 @@
 const express = require("express");
 const router = express.Router();
+
+/* GET graph page */
+router.get('/', (req, res, next) => {
+    res.render('graph_viewer');
+});
+
+module.exports = router;
