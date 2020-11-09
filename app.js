7aaf44c025a2431f62bfe81279376c9ed02b5e31
dpm24 <dpm2448dpm@gmail.com>
2020-11-09 20:59:25 +0100

    menu

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
+var multer= require('multer');
 
 var indexRouter = require('./routes/index');
 var grafo = require('./routes/grafo');
